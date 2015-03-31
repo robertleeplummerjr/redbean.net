d680746bf6cb718c29ed935cd86b66b8f2f13e02
Robert Plummer <robertleeplummerjr@gmail.com>
2015-03-30 21:31:05 -0400

    init

---
+++ build.bat
@@ -0,0 +1,7 @@
+..\Phalanger\Source\PhpNetCompiler\obj\Debug)\phpc ^
+/target:dll ^
+/pure+ ^
+/lang:CLR ^
+/out:RedBean.dll ^
+/recurse:../redbean\RedBeanPHP ^
+/r:../Phalanger\Source\ClassLibrary\obj\Debug)\PhpNetClassLibrary.dll 2>out.log
